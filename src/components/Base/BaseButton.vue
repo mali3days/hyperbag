d718e40c6002faf865b9510821a9b5cf1bf0031b
m3days <mali3days@gmail.com>
2019-03-24 23:39:32 +0200

    :sparkles: add BaseButton component

---
+++ src/components/Base/BaseButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-button
+    v-bind="$props"
+    @click="onClick"
+  >
+    <slot />
+  </el-button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class BaseButton extends Vue {
+  @Prop() private type!: string;
+
+  @Prop() private icon!: string;
+
+  @Prop() private plain!: boolean;
+
+  @Prop() private round!: boolean;
+
+  @Prop() private disabled!: boolean;
+
+  onClick() {
+    this.$emit('click');
+  }
+}
+</script>
