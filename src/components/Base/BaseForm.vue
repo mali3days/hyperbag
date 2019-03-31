f0af0008913b5c80bb60d9dcccbef5132c750d15
m3days <mali3days@gmail.com>
2019-04-01 00:02:54 +0300

    :sparkles: add BaseForm component

---
+++ src/components/Base/BaseForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-form
+    ref="form"
+    v-bind="$props"
+    @submit.native.prevent="$emit('submit', $event)"
+  >
+    <slot />
+  </el-form>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class BaseForm extends Vue {
+  @Prop() private model!: any;
+}
+</script>
