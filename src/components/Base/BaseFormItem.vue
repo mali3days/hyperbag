610032258df8eeb82d1e2b9ae227f79256036236
m3days <mali3days@gmail.com>
2019-04-01 00:02:12 +0300

    :sparkles: add BaseFormItem component

---
+++ src/components/Base/BaseFormItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-form-item
+    v-bind="$props"
+  >
+    <slot />
+  </el-form-item>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class BaseFormItem extends Vue {
+  @Prop() private label!: string;
+
+  @Prop() private prop!: string;
+}
+</script>
