0204d9a376329b4a7aed22b70611a3fd59b5e5e6
m3days <mali3days@gmail.com>
2019-04-01 00:00:51 +0300

    :sparkles: add BaseInput component

---
+++ src/components/Base/BaseInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-input
+    v-bind="$props"
+    @input="$emit('update:value', $event)"
+  >
+    <slot
+      v-for="(_, name) in $slots"
+      :name="name"
+      :slot="name"
+    />
+  </el-input>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class BaseInput extends Vue {
+  @Prop() private value!: string;
+
+  @Prop() private placeholder!: string;
+
+  @Prop() private clearable!: boolean;
+
+  @Prop() private disabled!: boolean;
+}
+</script>
