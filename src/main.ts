45109c05abf921de8c5a10996eee853d690f6c14
m3days <mali3days@gmail.com>
2019-04-07 13:12:48 +0300

    :sparkles: update main.ts file (store import and render method return type)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import Vue from 'vue';
+import Vue, { VNode } from 'vue';
+
 import App from '@/App.vue';
 import router from '@/router';
-import store from '@/store';
+import { store } from '@/store';
 
 import '../config/common';
 
@@ -12,5 +13,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: h => h(App),
+  render: (h): VNode => h(App),
 }).$mount('#app');
